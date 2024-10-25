614381fb16718793b50499cb479470b0d0072d97
ceciliawiklander <cecwik@kth.se>
2024-10-25 13:09:08 +0200

    Update d_h_meny.php

--- src/PI/DiVA/d_h_meny.php
+++ src/PI/DiVA/d_h_meny.php
@@ -1,60 +1,64 @@
-<?php session_start(); ?>
-
-<!DOCTYPE html PUBLIC "-//w3c//DTD XHTMLm 1.0 Transitional//EN"
-"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<?php 
+    session_start(); 
+    require_once('config.php.inc');
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml11/DTD/xhtml-transitional.dtd">
 
 <! Författare: Cecilia Wiklander>
 <! Syfte: DiVA-hantering>
 <! Ändringar: >
-<! Tagit bort menylänken till isbn-menyn ***** >
-<! Lagt till anv och lösen för åtkomst utan inloggning ***** >
-
-<html xmlns="http://www.w3.org/1999/xhtml">
 
 <head>
 
-<meta charset="utf-8">
+    <meta charset="utf-8">
 
-<title>MENY</title>
+    <title>MENY</title>
 
-<link href="Site_utan_storlek.css" rel="stylesheet">
+    <link href="Site_utan_storlek.css" rel="stylesheet">
 
-<?php include('include_diva.html');  ?>
-
-<?php
- try {
-            $anv = "pi_anv";
-            $ord = "V#puBL09";
-            $username = $anv;
-            $password = $ord;
-            $dbname = "hant_diva";
-            $dbh = new PDO("mysql:host=$hostname;dbname=$dbname", $username, $password);
-            $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $_SESSION['anv'] = $anv;
-            $_SESSION['ord'] = $ord;
-            $_SESSION['hnamn'] = $hostname;
-            $_SESSION['dbnamn'] = $dbname;
-            $_SESSION['granskad'] = "KONTROLL";
-        } catch (PDOException $e){
-            echo '<script language="javascript">';
-            echo 'alert("Det gick int att ansluta till databasen!")';
-            echo '</script>';
-        }
-
-?>
+    <?php include('include_meny.html'); ?>
 
 </head>
 
 <body>
 
-<br />
+<?php
+
+    $_SESSION['granskad'] = "";
+
+        try {
+            
+	    $username = "pi_anv";
+            $password = "V#puBL09";
+            $dbname = "hant_diva";
+            $dbh = new PDO("mysql:host=$hostname;dbname=$dbname", $username, $password);
+            $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $_SESSION['anv'] = $username;
+            $_SESSION['ord'] = $password;
+            $_SESSION['hnamn'] = $hostname;
+            $_SESSION['dbnamn'] = $dbname;
+            $_SESSION['granskad'] = "KONTROLL";		
+
+        } catch (PDOException $e){
+            echo '<script language="javascript">';
+            echo 'alert("Det gick inta att ansluta till databasen!")';
+            echo '</script>';
+        }   
+
+?>
+
+<br/>
+
+<br /><br />
 
 <ul>
 
 	<li><h3><a href='d_importmeny.php'>BEHANDLA IMPORTFIL</a></h3></li>
 	<li><h3><a href='d_soek_personal.php'>LETA KTH-ANSTÄLLDA</a></h3></li>	
 
-</ul>
+</ul>	
 
 </body>
+
 </html>
