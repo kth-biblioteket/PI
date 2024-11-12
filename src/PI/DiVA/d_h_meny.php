2a318a4cacfc8a0f0c2c569ed65afd7f050913c8
ceciliawiklander <cecwik@kth.se>
2024-11-12 11:04:44 +0100

    Update d_h_meny.php

--- src/PI/DiVA/d_h_meny.php
+++ src/PI/DiVA/d_h_meny.php
@@ -54,8 +54,7 @@
 
 <ul>
 
-	<li><h3><a href='d_importmeny.php'>BEHANDLA IMPORTFIL</a></h3></li>
-	<! <li><h3><a href='d_soek_personal.php'>LETA KTH-ANSTÄLLDA</a></h3></li> >	
+	<li><h3><a href='d_importmeny.php'>BEHANDLA IMPORTFIL</a></h3></li>		
 
 </ul>	
 
