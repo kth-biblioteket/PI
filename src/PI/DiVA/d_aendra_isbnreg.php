e78de219170bcab7140ecc7c5661a76f3c43e670
ceciliawiklander <cecwik@kth.se>
2024-11-19 15:45:36 +0100

    Update d_aendra_isbnreg.php

--- src/PI/DiVA/d_aendra_isbnreg.php
+++ src/PI/DiVA/d_aendra_isbnreg.php
@@ -84,10 +84,12 @@
 	                                                                                                    
 
             // ÄNDRA UPPG OM ISBN
+		
             $stmt = $pdo->prepare("UPDATE reg_isbn SET Titel = :Titel WHERE ISBN = :ISBN"); 
-            $stmt->bindParam(':ISBN', $ISBN);
-            $stmt->bindParam(':Titel', $Titel);
-            $stmt->execute();
+             $stmt->bindParam(':ISBN', $ISBN);
+             $stmt->bindParam(':Titel', $Titel);
+		echo $stmt;
+            // $stmt->execute();
              
             
             
