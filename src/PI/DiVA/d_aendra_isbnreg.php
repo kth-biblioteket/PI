208bd4538897df8e27d8e1abf3ea74ecfe615423
ceciliawiklander <cecwik@kth.se>
2024-11-21 15:49:41 +0100

    Update d_aendra_isbnreg.php

--- src/PI/DiVA/d_aendra_isbnreg.php
+++ src/PI/DiVA/d_aendra_isbnreg.php
@@ -61,17 +61,12 @@
             // ÄNDRA UPPG OM ISBN
 		
 
- $stmt = $pdo->prepare("SELECT ISBN FROM reg_isbn WHERE KTH_id = :KTHid");
-    $stmt->bindParam(':KTHid', $KTH_id);
-		
-    $stmt->execute();
+ 
 
-    foreach ($stmt as $row) {
-        $minstISBN = $row['ISBN'];        
-    }
-     echo  $minstISBN;
+    
 
 		echo $KTH_id;
+		echo $Titel;
              echo 'HEJSAN';
 
             echo '<script language="javascript">';
